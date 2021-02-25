d98d5b87bca3ea33c82284d20872f09a10f9cf55
toyobayashi <lifenglin314@outlook.com>
2021-02-25 17:36:35 +0800

    1

---
+++ README.md
@@ -0,0 +1,6 @@
+# vue3-playground
+
+```bash
+npm install
+npm run serve
+```
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { createApp, h } from 'vue'
+import App from './App.vue'
+// import App from './app'
+
+const app = createApp({
+  render: () => h(App)
+})
+
+app.mount('#app')
+
+// @ts-ignore
+if (module.hot) {
+  // @ts-ignore
+  module.hot.accept()
+}
